6441b16250280d65836dab1bf293bff2f4fd293b
yuda-lyu <firsemisphere@gmail.com>
2022-09-01 23:46:35 +0800

    modify: last zone in calcContours

--- src/calcContours.mjs
+++ src/calcContours.mjs
@@ -25,6 +25,14 @@ function calcContours(points, opt = {}) {
         // contours = [contours[0], contours[1], contours[2], contours[3]]
         // console.log('calcContours contours2', contours)
 
+        //偵測最末多邊形
+        let ub = size(contours) - 1
+        let contourLast = contours[ub]
+        let coordinates = get(contourLast, 'coordinates', []) //coordinates
+        if (size(coordinates) === 0) {
+            contours[ub].sepZone = 'top'
+        }
+
         return contours
     }
 
@@ -64,8 +72,8 @@ function calcContours(points, opt = {}) {
 
     //polylines
     let polylines = map(contours, (v, k) => {
-
         return {
+            sepZone: get(v, 'sepZone', ''),
             latLngs: v.coordinates,
             level: v.value,
             effectArea: getAreaMultiPolygonSm(v.coordinates),
@@ -78,14 +86,17 @@ function calcContours(points, opt = {}) {
     if (true) {
 
         //pmin, pmax
-        let pmin = min(map(polylines, 'level'))
-        let pmax = max(map(polylines, 'level'))
+        let ls = map(polylines, 'level')
+        let pmin = min(ls)
+        let pmax = max(ls)
         // console.log('pmin', pmin, 'pmax', pmax)
 
         //實際數據有超出contours的最大值, 添加虛擬polyline
         if (pmax < valueMax) {
+            console.log('非預期: pmax < valueMax', pmax < valueMax, pmax, valueMax)
             polylines.push({
                 mode: 'virtualEnd',
+                sepZone: '',
                 latLngs: [],
                 level: valueMax,
                 effectArea: 0,
@@ -94,8 +105,10 @@ function calcContours(points, opt = {}) {
 
         //實際數據有低於contours的最小值, 添加虛擬polyline
         if (pmin > valueMin) {
+            console.log('非預期: pmin > valueMin', pmin > valueMin, pmin, valueMin)
             let pl = {
                 mode: 'virtualStart',
+                sepZone: '',
                 latLngs: [],
                 level: valueMin,
                 effectArea: 0,
@@ -119,9 +132,16 @@ function calcContours(points, opt = {}) {
     let polygonSets = []
     for (let i = 0; i <= polylines.length - 2; i++) {
 
-        //p0,p1,range
-        let p0 = polylines[i].latLngs
-        let p1 = polylines[i + 1].latLngs
+        //p0, p1
+        let p0 = polylines[i]
+        let p1 = polylines[i + 1]
+        // console.log(i, 'p0', p0, p0.level, 'p1', p1, p1.level)
+
+        //ps0, ps1
+        let ps0 = p0.latLngs
+        let ps1 = p1.latLngs
+
+        //range
         let range = {
             text: `${polylines[i].level} - ${polylines[i + 1].level}`,
             low: polylines[i].level,
@@ -130,18 +150,22 @@ function calcContours(points, opt = {}) {
 
         //clipMultiPolygon
         let latLngs = []
-        latLngs = clipMultiPolygon(p0, p1)
-        if (get(polylines[i], 'mode' === 'virtualStart')) { //若為virtualStart, 則代表直接使用下1個polylines成為等值區域, 方能代表凹陷區
-            latLngs = p1 //因為既有屬性都是取前者, 故若virtualStart係使用後者p1, 就代表全部polygonSet都會有真實effectArea
+        latLngs = clipMultiPolygon(ps0, ps1)
+        if (p0.mode === 'virtualStart') { //若為virtualStart, 則代表直接使用下1個polylines成為等值區域, 方能代表凹陷區
+            latLngs = ps1 //因為既有屬性都是取前者, 故若virtualStart係使用後者p1, 就代表全部polygonSet都會有真實effectArea
+        }
+        else if (p1.sepZone === 'top') { //若後者sepZone為top, 代表無多邊形數據, 得要使用前者多邊形建構
+            latLngs = ps0
         }
         else {
-            latLngs = clipMultiPolygon(p0, p1)
+            latLngs = clipMultiPolygon(ps0, ps1)
         }
 
         polygonSets.push({
-            ...polylines[i],
+            ...p0,
             latLngs,
             range,
+            // sepZone:get(ps1)
         })
     }
     // console.log('polygonSets from polylines', cloneDeep(polygonSets))
