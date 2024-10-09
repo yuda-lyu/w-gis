c00bbe32becc874eef385be6ad94881222d2c5d9
yuda-lyu <firsemisphere@gmail.com>
2024-10-09 23:33:07 +0800

    modify: getBoxFromGeojson

--- g.mjs
+++ g.mjs
@@ -1,9 +1,11 @@
 
-import turf from './src/importTurf.mjs'
 import getBoxFromGeojson from './src/getBoxFromGeojson.mjs'
 
 
-let geojson = {
+let geojson
+let r
+
+geojson = {
     'type': 'FeatureCollection',
     'name': 'pgs',
     'features': [
@@ -49,9 +51,39 @@ let geojson = {
         },
     ]
 }
-let r
-
 r = getBoxFromGeojson(geojson)
 console.log(r)
+// => { xmin: 0, xmax: 2, ymin: 0, ymax: 2 }
+
+geojson = {
+    'type': 'Feature',
+    'properties': {},
+    'geometry': {
+        'type': 'MultiPolygon',
+        'coordinates': {
+            'type': 'Feature',
+            'properties': {
+                'name': 'pgs',
+            },
+            'geometry': {
+                'type': 'MultiPolygon',
+                'coordinates': [
+                    [
+                        [
+                            [0, 0],
+                            [-0.2, 1],
+                            [1, 1.1],
+                            [1, 0],
+                            [0, 0],
+                        ]
+                    ]
+                ]
+            }
+        }
+    }
+}
+r = getBoxFromGeojson(geojson)
+console.log(r)
+// => { xmin: -0.2, xmax: 1, ymin: 0, ymax: 1.1 }
 
 //node --experimental-modules g.mjs
